27d31bb489221fe1e27191576441e232d9d7f939
manoj0508 <manoj.k0508@gmail.com>
2026-06-18 01:02:14 +0530

    spring ai basic structure

---
+++ src/main/java/com/manoj/spring/ai/app/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.manoj.spring.ai.app.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("msg")String msg){
+        return chatClient.prompt(msg).call().content();
+    }
+}
---
+++ src/test/java/com/manoj/spring/ai/app/SpringOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.spring.ai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
