f6948b56b8817ad74502872d02f7f7956c2cb770
manoj0508 <manoj.k0508@gmail.com>
2026-06-25 22:30:17 +0530

    spring ai added config     added chat memory

--- src/main/java/com/manoj/spring/ai/app/config/ChatClientConfig.java
+++ src/main/java/com/manoj/spring/ai/app/config/ChatClientConfig.java
@@ -2,7 +2,12 @@ package com.manoj.spring.ai.app.config;
 
 import com.manoj.spring.ai.app.advisor.TokenUsageAdvisors;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,12 +25,28 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
         //var options = OpenAiChatOptions.builder().model("gpt-5-nano").temperature(1.0).build();
-
          return chatClientBuilder.defaultAdvisors(List.of(new SimpleLoggerAdvisor(),
                             new TokenUsageAdvisors()))
                 // .defaultOptions(options)
                 .defaultSystem(systemPromptTemplate)
                 .defaultUser("How can you help me ?")
                 .build();
+    }
+
+    @Bean(name = "jdbcH2ChatMemory")
+    public ChatMemory jdbcH2ChatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+        return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
+
+    @Bean(name = "memoryChatClient")
+    public ChatClient messageChatClient(ChatClient.Builder chatClientBuilder, ChatMemory jdbcH2ChatMemory){
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        MessageChatMemoryAdvisor messageChatMemoryAdvisor = MessageChatMemoryAdvisor.builder(jdbcH2ChatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, messageChatMemoryAdvisor))
+                .build();
+
+
     }
 }
---
+++ src/main/java/com/manoj/spring/ai/app/controller/ChatMemoryController.java
@@ -0,0 +1,29 @@
+package com.manoj.spring.ai.app.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ChatMemoryController {
+
+    private final ChatClient chatClient;
+
+
+    public ChatMemoryController(@Qualifier("memoryChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/chat-memory")
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username,
+                                             @RequestParam("message") String message) {
+
+       return ResponseEntity.ok(chatClient.prompt().user(message).advisors(
+                advisorSpec-> advisorSpec.param(ChatMemory.CONVERSATION_ID, username)).call().content());
+    }
+
+
+}
